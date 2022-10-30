14ebd4df629665d06cb4f4dec68546b4569b09fd
Suraj Kumar Singh <suraj.singh@primathon.in>
2022-10-30 14:05:45 +0530

    takes input in program

---
+++ take_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+ int a , b ;
+
+cout << "Enter Two Numbers" << endl;
+
+cin >> a >> b ;
+
+cout << "The sum of" << a << " and " << b << " is "<<a+b << endl;
+}
+
