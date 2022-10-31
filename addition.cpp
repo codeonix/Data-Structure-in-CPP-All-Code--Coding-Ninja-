c5fd13ccce2718e063d2ff027c1440ec1c3ab5d6
Suraj Kumar Singh <suraj.singh@primathon.in>
2022-10-31 08:57:23 +0530

    adds type cast

---
+++ addition.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+ int a = 10;
+char b = 'a';
+char ch = a+b;
+cout << a +b << (char)a+b<<ch<< endl; //Here no any typecast takes place 
+}
